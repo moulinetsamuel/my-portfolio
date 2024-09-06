b1269c8892d71c222b4828bc0b5ada890448862f
moulinetsamuel <samuel.moulinet@oclock.school>
2024-09-06 17:36:50 +0200

    Refactor folder structure and implement light/dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
-import { cn } from "@/libs/utils";
+import { cn } from "../libs/utils";
 import { Anek_Telugu } from "next/font/google";
-import { ThemeProvider } from "../src/libs/theme-provider";
+import { ThemeProvider } from "../providers/theme-provider";
 
 const AnekTelugu = Anek_Telugu({
   subsets: ["latin"],
